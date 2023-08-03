64f27f0d06922abc3df78238dfe0280301ae3b00
Betho <hgomescostajr@gmail.com>
2023-08-03 01:15:20 -0300

    add stock to products

--- resources/js/components/admin/products/ListProductsComponent.vue
+++ resources/js/components/admin/products/ListProductsComponent.vue
@@ -70,7 +70,7 @@ export default {
 
         pathImage(){
             const uri = window.location.host;
-            return 'https://' + uri + 'public/storage/';
+            return 'https://' + uri + '/public//storage/';
         },
 
         destroy(id){
